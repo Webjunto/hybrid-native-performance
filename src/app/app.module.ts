78464063d95d2260a9e6a7a886a2e94539fa23ce
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2017-07-12 00:40:58 +0530

    static pages comepleted with navigation flow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ItemsPage } from '../pages/items/items';
+import { ItemPage } from '../pages/item/item';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ItemsPage,
+    ItemPage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +23,14 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ItemsPage,
+    ItemPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/pages/items/items.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ItemPage } from '../item/item';
+
+@Component({
+  selector: 'page-items',
+  templateUrl: 'items.html',
+})
+export class ItemsPage {
+  public items = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.items = [
+      { "name": "item 1", "tasks": [] },
+      { "name": "item 2", "tasks": [] }
+    ];
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ItemsPage');
+  }
+
+  openItem(item) {
+    window.console.log(item);
+    this.navCtrl.push(ItemPage);
+  }
+
+  addItem() {
+    this.navCtrl.push(ItemPage);
+  }
+
+}
